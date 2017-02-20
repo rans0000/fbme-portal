6f3299807fa4f6a79e6070fe387386409280996a
rans0000 <rans0000@gmail.com>
2017-02-20 12:38:00 +0530

    Added * Rolemgmt: Create/Update dialog basic selecting & undeslecting permissions done.

--- dev/js/modules/app/app.core.constants.js
+++ dev/js/modules/app/app.core.constants.js
@@ -6,7 +6,7 @@
 
     angular.module('app.core.module')
         .constant('webServiceURL', getWebServiceURL())
-        .constant('permissions', getPermissions);
+        .constant('permissions', getPermissions());
 
     function getWebServiceURL () {
         var temp = {
@@ -19,17 +19,17 @@
     }
 
     function getPermissions () {
-        var temp = {
-            PERSONAL_DOCS: 1,
-            ADMIN: 2,
-            COLLABORATION_ADMIN: 4,
-            ROLE_MANAGEMENT: 8,
-            BRANCH_MANAGEMENT: 16,
-            DEPARTMENT_MANAGEMENT: 32,
-            SETTINGS_MANAGEMENT: 64,
-            USER_MANAGEMENT: 128,
-            USER_GROUP_MANAGEMENT: 256
-        };
+        var temp = [
+            {name: 'PERSONAL_DOCS', id: 1},
+            {name: 'ADMIN', id: 2},
+            {name: 'COLLABORATION_ADMIN', id: 4},
+            {name: 'ROLE_MANAGEMENT', id: 8},
+            {name: 'BRANCH_MANAGEMENT', id: 16},
+            {name: 'DEPARTMENT_MANAGEMENT', id: 32},
+            {name: 'SETTINGS_MANAGEMENT', id: 64},
+            {name: 'USER_MANAGEMENT', id: 128},
+            {name: 'USER_GROUP_MANAGEMENT', id: 256}
+        ];
         return temp;
     }
 
--- dev/js/modules/role/create-update/role.update.controller.js
+++ dev/js/modules/role/create-update/role.update.controller.js
@@ -6,9 +6,9 @@
     angular.module('app.core.module')
         .controller('RoleUpdateController', RoleUpdateController);
 
-    RoleUpdateController.$inject = ['$uibModalInstance', 'dialogData'];
+    RoleUpdateController.$inject = ['$uibModalInstance', 'dialogData', 'permissions'];
 
-    function RoleUpdateController ($uibModalInstance, dialogData) {
+    function RoleUpdateController ($uibModalInstance, dialogData, permissions) {
         var vm = this;
         vm.activeTab = 0;
         vm.data = dialogData;
@@ -16,19 +16,40 @@
             name: '',
             description: ''
         };
-        
+        vm.appPermissions = permissions;
+        vm.unselectedPermissions = {blue: false, green: true};
+        vm.selectedPermissions = {};
+
         vm.validateRoleDetail = validateRoleDetail;
+        vm.movePermissions = movePermissions;
         vm.ok = ok;
         vm.cancel = cancel;
-        
+
         //--------------------------------------
         //function declarations
-        
+
         function validateRoleDetail () {
             var isValid =  ((vm.role.name) && (vm.role.description))? true : false;
-            return isValid;
+            return !isValid;
         }
-        
+
+        function movePermissions (item1, item2, forceMove) {
+            if(!Object.keys(item1).length){
+                return;
+            }
+            for(var key in item1){
+                if(item1.hasOwnProperty(key)){
+                    if(item1[key] || forceMove){
+                        item2[key] = false;
+                        delete item1[key];
+                    }
+                    else{
+                        item1[key] = false;
+                    }
+                }
+            }
+        }
+
         function ok () {
             $uibModalInstance.close(vm.data.successResult);
         }
